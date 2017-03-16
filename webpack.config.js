3acb189a97f6feea2515bc4a174a07fa1b7c12c3
Jeka-Vasiliev <zheka_v@inbox.ru>
2017-03-16 07:47:30 +0300

    set babel and testing bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   entry: './index.js',
+  module: {
+    rules:[
+      { test: /\.js$/, loader: "babel-loader" }
+    ]
+  },
   output: {
     filename: 'build/bundle.js'
   }
